43991cec9575418a0d215ab03c8a32780fbd89f3
soniadonati <sonyd31@hotmail.com>
2016-12-13 11:07:22 +0100

    bug fixes

--- template.py
+++ template.py
@@ -246,7 +246,6 @@ def writelsbtoimage(image, bl):
     #New method
     import numpy as np
     
-    image = image.load()
     img_array = np.array(image)
     img_array.ravel()[:len(bl)] = setLSB(img_array.ravel()[:len(bl)], bl)
     image_output = Image.fromarray(img_array)
@@ -265,7 +264,6 @@ def getlsbfromimage(image):
     #New method
     import numpy as np
     
-    image = image.load()
     img_array = np.array(image)
     lsblist = getLSB(img_array.ravel()).tolist()
     
